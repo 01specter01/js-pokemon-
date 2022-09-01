18bc45bf58a55bb3c8971be18fdc79e3d9879d8b
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-01 10:26:29 +0200

    g

--- pokemon.js
+++ pokemon.js
@@ -5,9 +5,19 @@ class Pokemon {
         this.namen = namen;
         this.gesundheit = gesundheit;
         this.magie = magie;
-        this.Array = Array;
+        this.SkillsArray = Array;
+    }
+    learnAttackSkill(skill, wert) {
+        if (AttackSkill == truefy) {
+            return this.SkillsArray.push(skill, wert);
+        }
     }
 }
+
+let pikachu = new Pokemon("Pikatue", 400, 300, []);
+console.log(pikachu);
+pikachu.learnAttackSkill(`Dragon`, `50`);
+
 class AttackSkill {
     constructor(attacke, schadenhöhe, magie) {
         this.attacke = attacke;
@@ -17,3 +27,4 @@ class AttackSkill {
 }
 let lightning = new AttackSkill("lightning", 40, 30);
 console.log(lightning);
+pikachu.learnAttackSkill(lightning);
