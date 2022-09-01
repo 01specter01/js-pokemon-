ebf0782dcbb01da5fb55099e08759a324197abb1
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-01 16:21:47 +0200

    gt

--- pokemon.js
+++ pokemon.js
@@ -1,22 +1,38 @@
 console.log("Lets Play POKEMOOOONNNNNN!!!!!!!!!!!!!!!");
 
 class Pokemon {
-    constructor(namen, gesundheit, magie, Array) {
+    constructor(namen, gesundheit, magie) {
         this.namen = namen;
         this.gesundheit = gesundheit;
         this.magie = magie;
-        this.SkillsArray = Array;
+        this.skillsArray = [];
     }
-    learnAttackSkill(skill, wert) {
-        if (AttackSkill == truefy) {
-            return this.SkillsArray.push(skill, wert);
+    learnAttackSkill(skill) {
+        this.skillsArray.push(skill);
+        console.log(`${skill.attacke}`);
+        return `${this.namen} hat den skill ${skill.attacke} erlernt`;
+    }
+    showStatus() {
+        return `Pokemon ${this.namen} hat noch ${this.gesundheit} Gesundheit,${this.magie} Magie und ${this.skillsArray} Skills! `;
+    }
+    attack(opponent) {
+        if (this.magie > this.skillsArray[0].magie && opponent.gesundheit > 0) {
+            if (opponent.gesundheit > this.skillsArray[0].schadenhöhe) {
+                return `Pokemon ${this.namen} attackiert ${opponent.namen}. Pokomon ${this.namen} fügt ${opponent.namen} ${this.skillsArray[0].schadenhöhe} Schaden hinzu, aber ${opponent.namen} lebt noch!`;
+            } else {
+                opponent.gesundheit = 0;
+                return `Pokomon ${opponent.namen} ist gestorben`;
+            }
+        } else {
+            return `Pokemon ${this.namen} kann nicht ${opponent.namen} angreifen, er hat zu wenig magie! `;
         }
     }
 }
 
-let pikachu = new Pokemon("Pikatue", 400, 300, []);
+let pikachu = new Pokemon("Pikachu", 1200, 300, []);
 console.log(pikachu);
-pikachu.learnAttackSkill(`Dragon`, `50`);
+let zaidoor = new Pokemon("Zaidoor", 1500, 200, []);
+console.log(zaidoor);
 
 class AttackSkill {
     constructor(attacke, schadenhöhe, magie) {
@@ -27,4 +43,10 @@ class AttackSkill {
 }
 let lightning = new AttackSkill("lightning", 40, 30);
 console.log(lightning);
-pikachu.learnAttackSkill(lightning);
+console.log(pikachu.learnAttackSkill(lightning));
+let kampfschrei = new AttackSkill("kampfschrei", 200, 10);
+console.log(kampfschrei);
+console.log(zaidoor.learnAttackSkill(kampfschrei));
+console.log(pikachu.showStatus());
+console.log(pikachu.attack(zaidoor));
+console.log(pikachu.attack(zaidoor));
